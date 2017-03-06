8d0c77b0d59ce9618f78887bd1dcc2c4ba68f173
Bob Donderwinkel <b.donderwinkel@bdcreations.nl>
2017-03-06 17:30:23 +0100

    added product category overview page plus pagination and added Filament Group loadCSS

--- wp-content/themes/ted-and-tone/footer.php
+++ wp-content/themes/ted-and-tone/footer.php
@@ -19,6 +19,7 @@ if(is_woocommerce()){
 	TimberHelper::function_wrapper('get_woocommerce_term_meta');
 	TimberHelper::function_wrapper('get_post_thumbnail_id');
 	TimberHelper::function_wrapper('get_category_link');
+	TimberHelper::function_wrapper('get_permalink');
 	TimberHelper::function_wrapper('wp_get_attachment_image_url');
 	TimberHelper::function_wrapper('wp_get_attachment_image_srcset');
 	TimberHelper::function_wrapper('wp_get_attachment_image_sizes');
@@ -32,7 +33,7 @@ if(is_woocommerce()){
 		$timberContext['product'] = wc_get_product( $timberContext['post']->ID );
 		$terms = get_the_terms( $timberContext['post']->ID, 'product_cat' );
         $timberContext['category'] = get_term(reset($terms)->term_id, 'product_cat' );
-		$templates[] =  'page-shop-product.twig';
+		$templates[] = 'page-shop-product.twig';
 	}
 
 	if(is_shop()){
@@ -40,7 +41,6 @@ if(is_woocommerce()){
 		$shop_page_id = get_option( 'woocommerce_shop_page_id' );
 		$timberContext['post'] = Timber::get_post($shop_page_id);
 		$timberContext['intro'] = get_the_excerpt($shop_page_id);
-
 		$shop_categories = get_categories( array(
 	         'taxonomy'     => 'product_cat',
 	         'orderby'      => 'name',
@@ -50,13 +50,21 @@ if(is_woocommerce()){
 	         'title_li'     => '',
 	         'hide_empty'   => 0
 	  	));
-
 	  	$timberContext['categories'] = $shop_categories;
 		$templates[] =  'page-shop-home.twig';
 	}
 
 	if(is_product_category()){
-		$templates[] =  'page-shop-categories.twig';
+		TimberHelper::function_wrapper('wc_get_product');
+		TimberHelper::function_wrapper('wc_price');
+
+	 	$queried_object = get_queried_object();
+		$category_id = $queried_object->term_id;
+        $timberContext['category'] = get_term($category_id, 'product_cat' );
+        $timberContext['intro'] = term_description($category_id, 'product_cat' );
+        $timberContext['products'] = Timber::get_posts();
+        $timberContext['pagination'] = Timber::get_pagination();
+		$templates[] = 'page-shop-categories.twig';
 	}
 }else{
 	$templates[] =  'page-plugin.twig';
